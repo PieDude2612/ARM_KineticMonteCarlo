97809312180e424fb4c7cd62d59a4de6bbe7c840
PieDude2612 <arjun.viswanathan2612@gmail.com>
2020-04-25 12:24:33 -0400

    Final fixes and functionality added.

--- Simulation_Code/master.py
+++ Simulation_Code/master.py
@@ -1,3 +1,15 @@
+import importlib
+from Simulation_Code import simulator
+from Simulation_Code import propensity_fcn
+from Simulation_Code import reacRatesCalc
+from Simulation_Code import dataSetLoader
+from Simulation_Code import matchNcreate
+importlib.reload(simulator)
+importlib.reload(propensity_fcn)
+importlib.reload(reacRatesCalc)
+importlib.reload(dataSetLoader)
+importlib.reload(matchNcreate)
+
 from Simulation_Code.simulator import simulator
 
 totFiles = int(input("Enter total files to train from: "))
